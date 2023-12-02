1be559eacf912dce43e95358a319f6373dc549ea
actividad-7-arreglos <@https://github.com/AndreAguilar97/actividad-7-arreglos.git>
2023-12-02 14:14:19 -0500

    Avance 5 02/12/2023
    Se añadió el Login, el menú principal y se conectó con las demás ventanas, se añadió el boton cerrar sesión en el menú principal

---
+++ src/Menu.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame{
+    public JPanel MiPanel;
+    private JButton btnAltaDoctores;
+    private JButton btnAltaPacientes;
+    private JButton btnGenerarCita;
+    private JButton btnCerrarSesion;
+    
+    public Menu() {
+        btnAltaDoctores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                irDoctores();
+            }
+        });
+        btnAltaPacientes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                irPacientes();
+            }
+        });
+        btnCerrarSesion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cerrarSesion();
+            }
+        });
+    }
+
+    private void irDoctores(){
+            // Cerrar la ventana actual
+            this.dispose();
+
+            // Crear e mostrar la ventana de registro
+            Alta_Doctor altaDoctor = new Alta_Doctor();
+            JFrame frame = new JFrame("Alta_Doctor");
+            frame.setContentPane(altaDoctor.MiPanel);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // Ajusta según tus necesidades
+            frame.pack();
+            frame.setVisible(true);
+    }
+
+    private void irPacientes(){
+        // Cerrar la ventana actual
+        this.dispose();
+
+        // Crear e mostrar la ventana de registro
+        Alta_Paciente altaPaciente = new Alta_Paciente();
+        JFrame frame = new JFrame("Alta_Paciente");
+        frame.setContentPane(altaPaciente.MiPanel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // Ajusta según tus necesidades
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void cerrarSesion(){
+        this.dispose();
+        Login login = new Login();
+        JFrame frame = new JFrame("VLogin");
+        frame.setContentPane(login.MiPanel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // Ajusta según tus necesidades
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setContentPane(menu.MiPanel);
+        menu.setSize(500,500);
+        menu.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        menu.setVisible(true);
+    }
+}
